397e9bae484045c7fe9bccafb8ec974ed8cefb9f
programmer372 <dev@gabrielheinzer.ch>
2020-12-07 22:00:57 +0100

    Create nextion.py

---
+++ lib/nextion.py
@@ -0,0 +1,25 @@
+from machine import UART
+from os import uname
+import time
+class nextion:
+    WRITE_ONLY = 0
+    WRITE_AND_READ = 1
+    def __init__(self, tx_pin, rx_pin, baudrate):
+        self.uart = UART(1, baudrate, tx=tx_pin, rx=rx_pin)
+        self.uart.init(baudrate, bits=8, parity=None, stop=1)
+    def cmd(self, cmd, flags=WRITE_AND_READ):
+        end_cmd=b'\xFF\xFF\xFF'
+        self.uart.write(cmd)
+        self.uart.write(end_cmd)
+        if(flags == 1):
+            time.sleep_ms(100)
+            return self.uart.read()
+    def sleep(self, state):
+        self.cmd("sleep=" + str(state))
+    def page(self, page):
+        self.cmd("page " + str(page))
+    def reset(self):
+        self.cmd("rest")
+    def brightness(self, brightness):
+        self.cmd("dim=" + brightness)
+    
